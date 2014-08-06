34c7bac2d8f46130b5a7cda66cc472e9646ae024
unknown <sunnyn@cmu.edu>
2014-08-06 13:19:04 +0530

    Added comm from host to proxy

--- src/Proxy.java
+++ src/Proxy.java
@@ -4,47 +4,55 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.Arrays;
 
 public class Proxy {
 	public static void main(String args[])
 	{
-		int port = 65500;
+		int server_port = 65500;
 		if(args.length > 0)
-			port = Integer.parseInt(args[0]);
+			server_port = Integer.parseInt(args[0]);
 		try {
-			ServerSocket server = new ServerSocket(port);
+			ServerSocket server = new ServerSocket(server_port);
 			System.out.println(server.getLocalPort());
 			while (true) {
 				Socket connection = server.accept();
 				System.out.println(connection.toString());
 				try {
-					Reader in = new InputStreamReader(
-							new BufferedInputStream(
-									connection.getInputStream( )
-									)
-							);
-					StringBuffer request = new StringBuffer(4096);
-					int c = in.read();
-					while (c >=0) {
-						//System.out.print((char)(c));
-						if (c == '\r' || c == '\n' || c == -1)
-						{
-							System.out.println(request.toString());
-							request = new StringBuffer(4096);
-						}
-						else
-						{
-							request.append((char) c);
-						}
-						c = in.read();
+					Reader in = new InputStreamReader(new BufferedInputStream(connection.getInputStream()));
+					char[] rq = new char[8192];
+					int c = in.read(rq);
+					connection.close();
+					String request = String.valueOf(rq);
+					String[] header = request.split("\n");
+					String[] http_header = header[0].split(" ");
+					String method = http_header[0];
+					String url = http_header[1];
+					String footer = http_header[2];
+					if(method.equals("GET"))
+					{
+						URL u = new URL(url);
+						int port = u.getPort() == -1 ? 80 : u.getPort();
+						Socket dest = new Socket(u.getHost(), port);
+						Writer out = new OutputStreamWriter(dest.getOutputStream());
+						out.write(method + " " + url + " " + footer);
+						for(int i = 1; i<header.length; i++)
+							out.write(header[i]);
+						out.write("\r\n");
+						out.flush();
+						out.close();
+						dest.close();
 					}
-
+					else
+					{
+						System.out.println("DOESN'T Handle request.");
+						System.out.println(request);
+					}
 					Writer out = new OutputStreamWriter(connection.getOutputStream());
 					out.write("You've connected to this server. Bye-bye now.\r\n");
 					out.flush();
-					connection.close( );
+					//connection.close( );
 				}
 				catch (IOException ex) {
 
